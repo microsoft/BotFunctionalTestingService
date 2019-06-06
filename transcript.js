aef1aba82a2082035c2bced4267851846437ddc6
Guy Epshtein <t-guepsh@microsoft.com>
2019-06-06 14:39:11 +0300

    Handle adaptive cards - user's response data is now extracted from "value" attribute, too.

--- transcript.js
+++ transcript.js
@@ -5,11 +5,8 @@ class Transcript {
         var messages = (transcript && _.isArray(transcript)) ? _.where(transcript, { type: "message" }) : [];
         messages = _.map(messages, 
                          function(message) {
-                             let res = _.pick(message, ["type", "text", "attachments", "speak", "locale", "textFormat", "from", "recipient", "value"]);
-                             // if (res.hasOwnProperty("attachments") && res["attachments"][0].hasOwnProperty("content") && res["attachments"][0]["content"].hasOwnProperty("$schema")) {
-                             //     delete res["attachments"][0]["content"]["$schema"];
-                             // }
-                             return res;                           });
+                                    return _.pick(message, ["type", "text", "attachments", "speak", "locale", "textFormat", "from", "recipient", "value"]);
+                                });
         return messages;    
     }
 }
---
+++ transcriptTransformationScript.js
@@ -0,0 +1,41 @@
+/**
+ This script is used for removing attributes from a transcript exported by Bot Framework Emulator.
+ Since the Bot Framework Emulator is connected directly to the bot, and not through the Bot Connector,
+ there are differences between the messages in the exported transcript and the actual messages polled
+ by the Functional Tests application.
+ This script accepts one argument - a full path (including the file name) to a transcript file exported by
+ Bot Framework Emulator, and creates a new transformed transcript file named "'"transfromed.transcript"
+ in the script directory.
+ Transformation is done by applying all defined handlers on each entry of the transcript object.
+**/
+
+const fs = require('fs');
+
+/** Here we can add all the handlers we need **/
+// Handler 1
+function removeSchemaAttribute(currEntry) {
+    if (currEntry['type'] === 'message') {
+        if (currEntry.hasOwnProperty("attachments") && currEntry["attachments"][0].hasOwnProperty("content") && currEntry["attachments"][0]["content"].hasOwnProperty("$schema")) {
+            delete currEntry["attachments"][0]["content"]["$schema"];
+        }
+    }
+}
+
+/** This is the main function - It iterates over all entries of the transcript, and applies all handlers on each entry **/
+function main(path) {
+    let contentBuffer = fs.readFileSync(path);
+    let jsonTranscript = JSON.parse(contentBuffer);
+    for (let i = 0; i < jsonTranscript.length; i++) {
+        let currEntry = jsonTranscript[i];
+        // Here we call to all the handlers we defined
+        removeSchemaAttribute(currEntry);
+    }
+    try{
+        fs.writeFileSync('transfromed.transcript', JSON.stringify(jsonTranscript));
+    }catch (e){
+        console.log("Cannot write file ", e);
+    }
+}
+
+//Call main with file path as argument.
+main(process.argv[2]);
