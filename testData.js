791e8baf0793b213a7fe9f46002d5a21102a52c2
Guy Epshtein <t-guepsh@microsoft.com>
2019-08-07 15:35:34 +0300

    1. Changed var name.

--- testData.js
+++ testData.js
@@ -25,14 +25,14 @@ class TestData {
     }
 
     get secret() {
-        var parsedSecrets = null;
+        var parsedSecret = null;
         try {
-            parsedSecrets = JSON.parse(process.env['SECRETS'])[this.bot];
+            parsedSecret = JSON.parse(process.env['SECRETS'])[this.bot];
         }
         catch {
             throw new Error("Invalid format of bot secrets JSON");
         }
-        return parsedSecrets;
+        return parsedSecret;
     }
 
     static async fromRequest(request) {
